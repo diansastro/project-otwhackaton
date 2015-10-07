64980deff5257f84d9b412473bc8e5659e5586eb
Umar Wirahadikusuma <losstname@gmail.com>
2015-10-07 10:31:18 +0800

    Adding File

---
+++ TilangOnline/app/src/main/java/com/example/losstname/tilangonline/Login.java
@@ -0,0 +1,153 @@
+package com.example.losstname.tilangonline;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Login extends ActionBarActivity {
+    private EditText editTextUserName;
+    private EditText editTextPassword;
+    public static final String USER_NAME = "USERNAME";
+
+    public static String usernm;
+
+    String username;
+    String password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+
+    public void loginButton(View view) {
+        editTextUserName = (EditText) findViewById(R.id.userId);
+        editTextPassword = (EditText) findViewById(R.id.password);
+        username = editTextUserName.getText().toString();
+        password = editTextPassword.getText().toString();
+        login(username,password);
+        usernm = editTextUserName.getText().toString();
+    }
+
+    private void login(final String username, String password) {
+
+        class LoginAsync extends AsyncTask<String, Void, String> {
+
+            private Dialog loadingDialog;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loadingDialog = ProgressDialog.show(Login.this, "Please wait", "Loading...");
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                String uname = params[0];
+                String pass = params[1];
+
+                InputStream is = null;
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("user", uname));
+                nameValuePairs.add(new BasicNameValuePair("pass", pass));
+                String result = null;
+
+                try{
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpPost httpPost = new HttpPost("http://10.0.2.2/project-otwhackaton/cobalogin.php");
+                    httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                    HttpResponse response = httpClient.execute(httpPost);
+
+                    HttpEntity entity = response.getEntity();
+
+                    is = entity.getContent();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"), 8);
+                    StringBuilder sb = new StringBuilder();
+
+                    String line = null;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        sb.append(line + "\n");
+                    }
+                    result = sb.toString();
+                } catch (ClientProtocolException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String result){
+                String s = result.trim();
+                loadingDialog.dismiss();
+                if(s.equalsIgnoreCase("success")){
+                    Intent intent = new Intent(Login.this, Home.class);
+                    intent.putExtra(USER_NAME, editTextUserName.getText());
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(), "Invalid User Name or Password", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+        LoginAsync la = new LoginAsync();
+        la.execute(username, password);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ cobalogin.php
@@ -0,0 +1,25 @@
+<?php
+define('HOST','localhost');
+define('USER','root');
+define('PASS','root');
+define('DB','cmsprodi');
+ 
+$con = mysqli_connect('localhost','root','root','cmsprodi');
+ 
+$username = $_POST['user'];
+$password = $_POST['pass'];
+ 
+$sql = "select * from admin where user_id='$username' and password='$password'";
+ 
+$res = mysqli_query($con,$sql);
+ 
+$check = mysqli_fetch_array($res);
+ 
+if(isset($check)){
+echo 'success';
+}else{
+echo 'failure';
+}
+ 
+mysqli_close($con);
+?>
